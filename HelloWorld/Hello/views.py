f4399241ff4d7275aa03e71f1e14ea24add8853f
Edward Martínez <Alejed@gmail.com>
2020-11-16 11:53:11 -0500

    Commit Incial

---
+++ HelloWorld/Hello/views.py
@@ -0,0 +1,8 @@
+#Django
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def homePageView(request):
+    """Se encarga de mostrar la pagina inicial"""
+    return HttpResponse('Hola, World!')
